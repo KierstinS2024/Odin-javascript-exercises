de8434dd762b1501b7fb1924ee84da3723bd704b
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-06-22 17:52:58 -0600

    create temperature conversion function: - find the equations and assign the values to variables - round the variables to first decimal point - return values and test cases until all pass

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fDegrees) {
+  celsius = ((fDegrees - 32) * 5) / 9;
+  let cRounded = Math.round(celsius * 10) / 10;
+  return cRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cDegrees) {
+  farenheit = (cDegrees * 9) / 5 + 32;
+  let fRounded = Math.round(farenheit * 10) / 10;
+  return fRounded;
 };
+
+convertToFahrenheit(73.2);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
