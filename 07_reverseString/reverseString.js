cd49b18278681c3c4de503627f45a811dfbb70b6
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-06-22 13:24:16 -0600

    Complete reverseStrings.js run tests incrementally until all pass review readme and solution to see better coding practices

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function (word) {
+  //take word and break it up
+  let breakUpArray = word.split("");
+  //re-order letters
+  let reversedArray = breakUpArray.reverse();
+  //join back together
+  let reversedString = reversedArray.join("");
 
+  return reversedString;
 };
+
+// WAY easier verson:
+// const reverseString = function (string) {
+//   return string.split("").reverse().join("");
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
