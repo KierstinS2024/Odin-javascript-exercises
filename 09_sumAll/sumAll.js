d204defdaef758fe935e4d37f45bd751d21edaae
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-06-22 15:13:05 -0600

    create a function sumAll that takes 2 integers create conditions to make sure only integers are accepted create conditions to make sure no negatives are accepted create conditions to start from smallest to largest numbers, regardless of which arguement is bigger make variable 'sum' to add to during loops increment by one and return 'sum'

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  //add conditions to make sure there's no negatives and only accepts integers
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
+  //create variable to add to
+  let sum = 0;
+  //make sure we're starting with the smaller number and increasing to larger number
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
