af906dd3683d0656a8f53644d0ba52de5e02e96f
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-07-24 23:37:14 -0600

    Exercise 13 palindromes: create pseudocode to determine next steps

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
 const palindromes = function () {
-
+  //take string
+  //break up string
+  // either push into new array reverse and join OR
+  // += string to new variable and determine if string === new variable
+  // Create if statement for true/false conditions
 };
 
 // Do not edit below this line
