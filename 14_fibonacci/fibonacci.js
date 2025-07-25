e4589eaed9052370000722715476d308615f9660
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-07-25 00:56:10 -0600

    Exercise 14: fibonacci pseudocode

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+// function fibonacci(n) {
+//   if n is 1 or 2:
+//     return 1
 
-};
+//   otherwise:
+//     set two starting values (like prev and current)
+//     loop from 3 up to n:
+//       calculate the next number in the sequence
+//       update previous values
+
+//     return the result
+// }
+
+const fibonacci = function () {};
 
 // Do not edit below this line
 module.exports = fibonacci;
