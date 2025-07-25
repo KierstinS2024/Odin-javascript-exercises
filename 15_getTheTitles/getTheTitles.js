eaa082d42b9292e7e87072dbe6f57f3c22efa07f
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-07-25 02:30:53 -0600

    Exercise 15: Extract book titles using map Return array of titles from list of book objects Use descriptive variable name for clarity

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (books) {
+  const bookList = books.map((book) => book.title);
+  return bookList;
 };
 
 // Do not edit below this line
